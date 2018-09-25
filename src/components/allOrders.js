98bdef448f3134fffec1e9b84649197e393026b9
Rob <rbirchtree@gmail.com>
2018-09-25 16:21:13 -0700

    added styling for allorders.js and fixed indexing

--- src/components/allOrders.js
+++ src/components/allOrders.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import _ from "lodash";
 import { connect } from 'react-redux';
-import { getCurrentOrders } from '../actions/orders'
+import { getCurrentOrders } from '../actions/orders';
+import './allOrders.css';
 //import { getCurrentOrders } from '../actions/orders';
 
 class AllOrders extends Component {
@@ -12,7 +13,14 @@ class AllOrders extends Component {
 	renderOrders() {
 		return _.map(this.props.orders, order => {
 			return (
-				<li>{order.deliveryDate}</li>
+				<ul className="order" key={order.toString()}>
+					<li className="orderInfo">Delivery Date: <span className="orderInfoSpan">{order.deliveryDate}</span></li>
+					<li className="orderInfo">Delivery Place: <span className="orderInfoSpan">{order.deliveryPlace}</span></li>
+					<li className="orderInfo">To: <span className="orderInfoSpan">{order.giftTo}</span></li>
+					<li className="orderInfo">Gift: <span className="orderInfoSpan">{order.gift}</span></li>
+					<li className="orderInfo">Instructions: <span className="orderInfoSpan">{order.instructions}</span></li>
+					<li className="orderInfo">Superhero: <span className="orderInfoSpan">{order.superhero}</span></li>
+				</ul>
 				)
 		})
 	}
@@ -21,9 +29,9 @@ class AllOrders extends Component {
 		return(
 			<div>
 				<h3>Your Orders</h3>
-				<ul>
+				
 					{this.renderOrders()}
-				</ul>
+				
 			</div>
 			)
 	}
