5396a7354c5a1caf977d4289d4bc32f86595a7d0
rbirchtree <rbirchtree@gmail.com>
2018-11-29 10:05:42 -0800

    added keys to gitignore, secured keys for best practice

--- src/components/SecureCheckOut.js
+++ src/components/SecureCheckOut.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Elements, StripeProvider} from 'react-stripe-elements';
 import CheckoutForm from './CheckoutForm';
-import {stripeAPIKEY} from '../config';
+import {stripeAPIKEY} from './keys';
 
 class SecureCheckOut extends Component {
   render() {
