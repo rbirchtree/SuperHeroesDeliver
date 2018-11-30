1720fc5429113f34ac15bb8a9948e2d1ef4ab58b
Robert Birch <rbirchtree@gmail.com>
2018-11-30 11:16:23 -0800

    removing
    removing due to modify config settings with keys

--- src/config.js
+++
@@ -1,6 +0,0 @@
-export const API_BASE_URL =
-    process.env.REACT_APP_API_BASE_URL || 'https://fierce-oasis-81437.herokuapp.com/api';
-    //below are two different urls for local host api
-    //1 http://localhost:8080/api 
-    //2 https://fierce-oasis-81437.herokuapp.com/api
-
