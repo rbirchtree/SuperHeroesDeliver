6eafa62617af4a1ca717e21222c83214758ec194
rbirchtree <rbirchtree@gmail.com>
2018-12-13 08:10:40 -0800

    added more images of superheroes

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
 export const API_BASE_URL =
     process.env.REACT_APP_API_BASE_URL || 'https://fierce-oasis-81437.herokuapp.com/api';
+    //http://localhost:8080/api https://fierce-oasis-81437.herokuapp.com/api
 export const stripeAPIKey = "pk_live_0L4kaakOUalrBozz25j0FOAu";
+
+//http://localhost:3000/
