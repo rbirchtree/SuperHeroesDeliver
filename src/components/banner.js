4fdf2be2a7540cc68b46819b040e4520743ad06b
rbirchtree <rbirchtree@gmail.com>
2019-01-03 14:11:03 -0800

    minimized img css, added 75 to banner

--- src/components/banner.js
+++ src/components/banner.js
@@ -4,7 +4,7 @@ import './banner.css';
 const Banner = () => 
 	 (
 		<div className="banner">Can't cheer up your love one? <br/>A Superhero can!
-             <span className="smiley">&#9786;</span></div>
+             <span className="smiley">&#9786;</span> for $175</div>
 )
 
 export default Banner;
