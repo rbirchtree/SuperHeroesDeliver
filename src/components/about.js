dba6ba6b3c2a6cb9dfaa04fdea37bcddd3f4b7f4
rbirchtree <rbirchtree@gmail.com>
2019-01-04 15:51:34 -0800

    fixedname on about page

--- src/components/about.js
+++ src/components/about.js
@@ -9,10 +9,10 @@ export default function About(){
 			<div class="container">
 					<h1 className="aboutTitle">About</h1>
 			</div>
-			<p className="about"><span className="logo">Superheroes Delivers</span><span className="punction">!</span> is a non sequitur way to deliver laughter to children
+			<p className="about"><span className="logo">Superheroes Deliver</span><span className="punction">!</span> is a non sequitur way to deliver laughter to children
 			 and adults who are in the hospital for a prolong period of time, or whatever reason. Flowers are nice, 
 			 but they are better when delivered by a superhero. 
-			 <span className="logo">Superheroes Delivers</span><span className="punction">!</span> was founded by Rob Birch. 
+			 <span className="logo">Superheroes Deliver</span><span className="punction">!</span> was founded by Rob Birch. 
 			 If you have issues, please call (512) 515-1628.</p>
 			 <ul className="footer">
                 <li><Link className="demoAbout" to="/about">About</Link></li>
