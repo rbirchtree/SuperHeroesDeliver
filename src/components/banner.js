6192bf2f2bb1db661d1dd53869f509cde9282746
rbirchtree <rbirchtree@gmail.com>
2019-01-04 08:31:08 -0800

    fixed banner

--- src/components/banner.js
+++ src/components/banner.js
@@ -3,8 +3,8 @@ import './banner.css';
 
 const Banner = () => 
 	 (
-		<div className="banner">Can't cheer up your love one? <br/>A Superhero can!
-             <span className="smiley">&#9786;</span> for $175</div>
+		<div className="banner">Can't cheer up your love one? <br/>A Superhero can
+              for $175! <span className="smiley">&#9786;</span></div>
 )
 
 export default Banner;
