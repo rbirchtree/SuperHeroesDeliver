362a06ce42463db7d075cd0a4c1ce7128b7b91e4
Rob Birch <rbirchtree@gmail.com>
2019-07-16 11:32:46 -0700

    fixed endpoint for stripe charge

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const API_BASE_URL =
-    process.env.REACT_APP_API_BASE_URL || 'https://superheroesdeliverserver.herokuapp.com/api/stripe/charge';
+    process.env.REACT_APP_API_BASE_URL || 'https://superheroesdeliverserver.herokuapp.com/api';
 export const stripeAPIKey = "pk_live_vtVDsFJjoX417gqmdyamimQb00pRxUP8Lx";
 
